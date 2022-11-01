49f7e31a1445608ac086f6d5014032d5d6645180
sqweeddy <ex1smor@gmail.com>
2022-11-01 09:35:27 +0600

    fix empty input submit

--- src/components/TodoMaker/TodoMaker.tsx
+++ src/components/TodoMaker/TodoMaker.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useRef, useState } from "react";
 import { Button } from "../Button";
 import styles from "./todomaker.module.css";
 
@@ -8,6 +8,8 @@ interface ITodoMaker {
 
 export function TodoMaker({ handleAddTodo }: ITodoMaker) {
   const [value, setValue] = useState({ name: "" });
+  const [error, setError] = useState<null | string>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue({ name: e.target.value });
@@ -15,6 +17,11 @@ export function TodoMaker({ handleAddTodo }: ITodoMaker) {
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    if (inputRef.current && inputRef.current.value.length < 1) {
+      setError("Enter todo title");
+      return;
+    }
+    setError(null);
     handleAddTodo(value);
     setValue({ name: "" });
   };
@@ -29,8 +36,10 @@ export function TodoMaker({ handleAddTodo }: ITodoMaker) {
           name="name"
           value={value.name}
           onChange={handleChange}
+          ref={inputRef}
           data-testid="input"
         />
+        {error && <p className={styles.error}>{error}</p>}
         <Button color="blue">ADD Todo</Button>
       </form>
     </div>
